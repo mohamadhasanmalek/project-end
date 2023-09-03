397ed364e35f7674130b66277f4252f540509b83
mohamadhassan <mohamadhasanmalkian@gmail.com>
2023-09-03 14:12:58 +0430

    create data base

--- app.py
+++ app.py
@@ -8,4 +8,21 @@ from wtforms.validators import InputRequired, Length
 from wtforms.widgets import TextArea
 from flask_bcrypt import Bcrypt
 import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thesecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/picture'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+with app.app_context():
+    db = SQLAlchemy(app)
+    bcrypt = Bcrypt(app)
+
+post_tag = db.Table('post_tag',
+                    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                    )
